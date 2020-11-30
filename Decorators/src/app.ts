be5308ee7fb00dce6be78e7751a64bed8b9986b1
JohnCowNut <thuonghamno3xz@gmail.com>
2020-11-30 14:40:54 +0700

    Add auto bind layer upto class by decorators

--- Decorators/src/app.ts
+++ Decorators/src/app.ts
@@ -97,3 +97,31 @@ class Product {
 // ORDER IN CLASS  // execution define the class
 const p1 = new Product('Book', 19)
 const p2 = new Product('Book2 ', 32)
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    // automatic set this key word
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+
+            const boundFn = originalMethod.bind(this);
+            return boundFn
+        }
+    }
+    return adjDescriptor
+}
+class Printer {
+    message = 'This Work!';
+    @Autobind
+    showMessage() {
+        console.log(this.message)
+    }
+}
+const p = new Printer()
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', p.showMessage)
+// Show undefined because , the "this" is not ref the content
+
+// Using decorators bind this
