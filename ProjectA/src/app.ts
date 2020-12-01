85292fde65377f2210c063af538dd9d828451896
JohnCowNut <thuonghamno3xz@gmail.com>
2020-12-01 13:59:36 +0700

    add class Project input prepare for pet project

---
+++ ProjectA/src/app.ts
@@ -0,0 +1,20 @@
+class ProjectInput {
+
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.attach()
+        console.log(this.element)
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+}
+const test1 = new ProjectInput()
