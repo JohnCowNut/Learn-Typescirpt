5b7adc9ced4a40bf2f2d4f4bf359df29c98360f9
JohnCowNut <thuonghamno3xz@gmail.com>
2020-12-01 15:37:10 +0700

    add practice validate customer

--- ProjectA/src/app.ts
+++ ProjectA/src/app.ts
@@ -1,24 +1,77 @@
 // Decorator
+
+/*
+* === Start:  (1):Auto Bind ===
+*/
 function AutoBind(_: any, _2: string | Symbol, description: PropertyDescriptor) {
 
     const originalMethod = description.value;
     const adjDescriptor: PropertyDescriptor = {
         configurable: true,
         enumerable: false,
+        // try access the function at bottom 
         get() {
             return originalMethod.bind(this)
         }
     }
     return adjDescriptor
 }
+/**
+ * End Auto bind 
+ */
+
+
+/* === Start:  Validate=== */
+interface ValidatorConfig {
+    [property: string]: {
+        [validatableProp: string]: string[] // ['required', 'positive']
+    }
+}
+// DECORATORS FOR BINDING 
+const registeredValidators: ValidatorConfig = {}
+function Required(target: any, propertyName: string) {
+    console.log("=== (1) ===  Required")
+
+    registeredValidators[target.constructor.name] = {
+        ...registeredValidators[target.constructor.name],
+        [propertyName]: ['required']
+    }
+}
+function validate(obj: any) {
+    const objValidateConfig = registeredValidators[obj.constructor.name];
+    if (!objValidateConfig) {
+        return true;
+    }
+    let isValid = true;
+
+    for (const prop in objValidateConfig) {
+        for (const validator of objValidateConfig[prop]) {
+
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop]
+                    break;
+            }
+        }
+    }
+    return isValid;
+}
+/**
+ * === End: Validate
+ */
+
+
 // Project Input
 class ProjectInput {
 
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    @Required
     titleElementInput: HTMLInputElement;
+    @Required
     descriptionInputElement: HTMLInputElement;
+    @Required
     peopleInputElement: HTMLInputElement;
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -29,7 +82,7 @@ class ProjectInput {
         this.element = importedNode.firstElementChild as HTMLFormElement;
 
         this.element.id = 'user-input'
-        console.log(this.element)
+
 
         this.titleElementInput = this.element.querySelector('#title')! as HTMLInputElement
         this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
@@ -41,8 +94,38 @@ class ProjectInput {
     private submitHandler(event: Event) {
         // prevent default submission 
         event.preventDefault();
-        console.log(this.titleElementInput)
-        console.log(this.titleElementInput.value)
+        const userInput = this.gatherUserInput();
+        // check if userInput is Tuple
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people)
+            this.clearInputs()
+        }
+    }
+    private clearInputs() {
+        this.titleElementInput.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleElementInput.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+        const obj = {
+            titleElementInput: enteredTitle,
+            descriptionInputElement: enteredDescription,
+            peopleInputElement: enteredPeople
+        }
+        if (validate(obj)) {
+            alert('Invalid Input Please Try Again');
+            return;
+        } else {
+            // convert to tuple +enteredPeople to number by +
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        }
+
     }
 
     private configure() {
@@ -50,6 +133,7 @@ class ProjectInput {
         // @params: đây cũng là 1 một cách nhưng chúng ta đã học decorators rồi thì phải chiến thôi hêh 
         //this.element.addEventListener('submit', this.submitHandler.bind(this))
         this.element.addEventListener('submit', this.submitHandler)
+
     }
 
     private attach() {
