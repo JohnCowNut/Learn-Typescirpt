c38f253a70257453614d59ec4c109743becf25f3
JohnCowNut <thuonghamno3xz@gmail.com>
2020-12-02 14:33:20 +0700

    filter base on status project and remove duplicate when render

--- ProjectA/src/app.ts
+++ ProjectA/src/app.ts
@@ -133,7 +133,13 @@ class ProjectList {
         this.element.id = `${type}-projects`;
         // resigter listener 
         projectState.addListener((projects: Project[]) => {
-            this.assignedProjects = projects;
+            const relevantProjects = projects.filter(prj => {
+                if (this.type === 'active') {
+                    return prj.status === ProjectStatus.active
+                }
+                return prj.status === ProjectStatus.finished
+            })
+            this.assignedProjects = relevantProjects;
             this.renderProject();
         })
         this.attach();
@@ -141,6 +147,8 @@ class ProjectList {
     }
     private renderProject() {
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        // Prevent for duplicate content 
+        listEl.innerHTML = ''
         for (const prjItem of this.assignedProjects) {
             const listItem = document.createElement('li');
             listItem.textContent = prjItem.title;
